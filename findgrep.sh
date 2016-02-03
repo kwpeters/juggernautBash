af286ad1ebd50f937babbdac8318d4acf2e564d5
Kevin Peters <kwpeters@ra.rockwell.com>
2016-02-03 07:41:15 -0500

    Added third arg to findgrep.sh to specify num context lines.

--- findgrep.sh
+++ findgrep.sh
@@ -5,10 +5,19 @@
 # The first argument is a regular expression that determines the set of files to be
 # searched.
 #
-# The second argument is a regular expression for the text to be searched for within each
-# file.
+# The second argument (optional) is a regular expression for the text to be searched for
+# within each file.
+#
+# The third argument (optional) is the number of context lines to output above and below
+# each match.  If not specified, a default of 5 is used.
 
 # Note: The last grep is used to colorize the output.  It is not absolutely necessary.
+
+NUM_CONTEXT_LINES=5
+if ! [ -z "$3" ]; then
+    # The number of context lines was specified.
+    NUM_CONTEXT_LINES=$3
+fi
 
 
 if [ -z "$2" ]; then
@@ -16,5 +25,5 @@ if [ -z "$2" ]; then
     find . -iregex $1 -print
 else
     # Both a file name regex and a text regex were specified.
-    find . -iregex $1 -exec grep -Hin -C 5 $2 {} \; 2> /dev/null | grep --color=auto -i -C 5 $2
+    find . -iregex $1 -exec grep -Hin -C $NUM_CONTEXT_LINES $2 {} \; 2> /dev/null | grep --color=auto -i -C $NUM_CONTEXT_LINES $2
 fi

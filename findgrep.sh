7bd0a5127ca91ffc7045d56454f44bb51bea0e92
Kevin Peters <kwpeters@ra.rockwell.com>
2016-02-03 22:06:23 -0500

    Fixed quoting in findgrep.sh.

--- findgrep.sh
+++ findgrep.sh
@@ -22,8 +22,11 @@ fi
 
 if [ -z "$2" ]; then
     # No text regex was specified.  We will just find file names that match.
-    find . -iregex $1 -print
+    echo "---"
+    echo "$1"
+    echo "---"
+    find . -iregex "$1" -print
 else
     # Both a file name regex and a text regex were specified.
-    find . -iregex $1 -exec grep -Hin -C $NUM_CONTEXT_LINES $2 {} \; 2> /dev/null | grep --color=auto -i -C $NUM_CONTEXT_LINES $2
+    find . -iregex "$1" -exec grep -Hin -C $NUM_CONTEXT_LINES "$2" {} \; 2> /dev/null | grep --color=auto -i -C $NUM_CONTEXT_LINES "$2"
 fi
